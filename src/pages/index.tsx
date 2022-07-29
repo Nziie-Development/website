36277a0ea130edfbfaa3560c9f9f59f6548f1099
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2022-07-29 07:44:17 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,9 +19,7 @@ export default function Home(): JSX.Element {
       />
       <main className="content-wrap--home-page">
       I provide cheap Discord bot services - a Pycord, HTML, CSS, JS, CSS and SCSS Developer. Imagine a custom made to your liking...
-    <p>I offer mainly custom Discord bots. You can contact me on my Discord server <a target="_blank" href="/discord" >Nziie Development</a> <!-- or directly using Discord DMs - <a target="_blank" href="https://discordapp.com/users/550476809280421903">vNziie--#7777</a> -->.</span><br /> <span>I also have one public bot - <a target="_blank" href="https://servermanagerbot.ml/invite">Server Manager</a>. Server Manager is a multipurpose bot, growing in features but is mainly a simple moderation bot with many customizations.</p>
-     <p id="text02"><span>I code all Discord bots in Python (pycord library - discord.py v2 fork).</span><br /> <span>In a partnership with <a target="_blank" href="https://anondev.ml">AnonDev</a> I offer cheap hosting for the custom bots on powerful servers with good uptime. If you are interested in getting custom bot from me, please join <strong><a target="_blank" href="/discord" >Nziie Development</a> Discord server and open ticket </strong>. If you are interested in hosting your Discord bot on <em>AnonDev&#39;s hosting service</em>, please join the support server and open new support ticket. 
- </main>
+      </main>
     </DefaultLayout>
   );
 }
