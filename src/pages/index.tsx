444238b10836f8442a637ad641366bdf7b2fe435
AnonDev <85408287+anondev-sudo@users.noreply.github.com>
2022-07-29 10:22:38 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import DefaultLayout from "../layouts/DefaultLayout";
 import PYCHero from "@site/src/components/PYCHero";
 import PYCButton from "@site/src/components/Button";
+import Head from '@docusaurus/Head';
 
 export default function Home(): JSX.Element {
   return (
@@ -9,6 +10,9 @@ export default function Home(): JSX.Element {
       title="Nziie"
       description="Nziie Development services"
     >
+<Head>
+<script async src="https://arc.io/widget.min.js#hD9CJ6sv"></script>
+</Head>      
       <PYCHero
         title="Nziie's Website"
         subtitle="Imagine a cheap custom bot made to your liking"
