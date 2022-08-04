1914a1a42d2bdd857237270d86a5225dccac0676
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2022-08-03 18:37:27 -0700

    Update PYCHero.tsx

--- src/components/PYCHero.tsx
+++ src/components/PYCHero.tsx
@@ -24,11 +24,3 @@ const PYCHero: React.FC<PYCHeroProps> = ({ title, subtitle, hasLogo = true }) =>
                 <div className="pyc-hero__actions">
                     <PYCButton type="filled inverted wspace" link="/introduction">Introduction</PYCButton>
                     <PYCButton type="filled inverted wspace" link="/services">Services</PYCButton>
-                    <PYCButton type="filled inverted wspace" link="/reviews">Reviews</PYCButton>
-                </div>
-            </div>
-        </section>
-    );
-};
-
-export default PYCHero;
