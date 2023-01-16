63eeb5ddddabbb15b95df66053ea7c7599bae850
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2023-01-16 08:25:39 -0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,7 +53,7 @@ const config = {
       hideOnScroll: true,
       items: [
         {
-          href: 'discord.html',
+          href: 'discord',
           position: 'right',
           label: 'Discord',
         },
